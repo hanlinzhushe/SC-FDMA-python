1b85c423de907322113d8f67f6a53aacef82a277
jarandacu <bary.bary@live.cl>
2017-11-06 16:29:00 -0300

    Tercero !

--- SC-FDMA.py
+++ SC-FDMA.py
@@ -1,3 +1,22 @@
 # noinspection PyUnresolvedReferences
 import numpy as np
 
+## Functions
+
+def Mod(Numbersymbols,modulation):
+    if modulation=="BPSK":
+        n=Numbersymbols
+    elif modulation=="QPSK":
+        n=Numbersymbols*2
+    elif modulation=="16-QAM":
+        n=Numbersymbols*4
+    x = np.random.randint(0, 2, n)
+    return x
+
+## Parameters
+mod="16-QAM"
+nsym=100
+print(Mod(nsym,mod))
+
+
+
